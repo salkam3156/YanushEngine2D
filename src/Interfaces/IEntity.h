7e5cf1952dec383c2e0074b8a389758879dccca6
tushwintz <salkam3156@hotmail.com>
2018-09-29 20:32:55 +0200

    extracting IEntity interface

--- src/Interfaces/IEntity.h
+++ src/Interfaces/IEntity.h
@@ -1,7 +1,25 @@
 #pragma once
 #include "../stdafx.h"
 
-class IEntity : public sf::Drawable
+class IEntity
 {
+public:
+	enum class EntityType
+	{
+		PLAYER,
+		ENEMY
+	};
 
+public:
+	virtual void Move() = 0;
+	virtual float Turn(sf::Vector2i position) = 0;
+	virtual void Scale(float delta) = 0;
+	virtual void Draw(sf::RenderTarget & target, sf::RenderStates states = sf::RenderStates::Default) const = 0;
+	virtual sf::Vector2f GetPosition() = 0;
+
+
+public:
+	IEntity(const IEntity& rhs) = delete;
+	IEntity() {};
+	~IEntity() {}
 };
